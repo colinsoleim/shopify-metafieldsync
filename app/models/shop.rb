f2cb4fd51a12999cd212875a077d13dcb5aecd5a
colinsoleim <soleimc@gmail.com>
2019-06-04 13:05:19 -0400

    Initial work in progress for metafield sync job

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -13,4 +13,12 @@ class Shop < ActiveRecord::Base
     )
     ShopifyAPI::Base.activate_session(session)
   end
+
+  def pull_metafields_from(shop, scope="all")
+    SyncMetafields.call(
+      pull_metafields_into_shop: self,
+      pull_metafields_from_shop: shop,
+      scope: scope
+    )
+  end
 end
--- app/services/sync_metafields.rb
+++ app/services/sync_metafields.rb
@@ -1,7 +1,28 @@
 class SyncMetafields
   include Service
 
-  def call
-    true
+  def call(options)
+    metafields_to_sync = build_metafields_to_sync(options)
+    
+    shop = pull_metafields_into_shop
+
+    metafields_to_sync.each do |metafield|
+      shop.add_metafield(
+        name: metafield.name,
+        other_variables: metafield.other_variables
+      )
+    end
+  end
+
+  def build_metafields_to_sync(options)
+    if options["scope"] == "all"
+      metafields_to_sync = shop.get_all_metafields
+    elsif options["scope"] == "all-products"
+      metafields_to_sync = shop.get_all_product_metafields
+    elsif options["scope"] == "all-products-and-variants" 
+      metafields_to_sync = shop.get_all_product_metafields + shop.get_all_variant_metafields
+    elsif options["scope"].is_a? ShopifyProduct
+      metafields_to_sync = shopify_product.get_all_metafields
+    end
   end
 end
